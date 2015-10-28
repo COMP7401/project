16e8dd6f42f8196508e4e11295c609cf3554994c
HarvP19 <h_powar19@hotmail.com>
2015-10-27 19:34:36 -0700

    Remote Changes

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -18,10 +18,78 @@ import java.io.*;
  * @author Harv, Callum, Kyle
  */
 public class RemoteControl {
+    public static final int FWD = 0;
+    public static final int STOP = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
+    public static final int FLAG_SIGNAL = 4;
+    public static final int CONTROLLER_DEVICE = 5;
+    public static final int FLAG_DEVICE  = 6;
+    private NXTConnection racecarConnection;
+    private DataOutputStream racecarOutputStream;
+    private Button leftButton;
+    private Button rightButton;
+    private Button centerButton;
+    private Button bottomButton;
+    
+    
     /**
      * Main entry to the program.
      */
     public static void main(String[] args) {
+        RemoteControl remote = new RemoteControl();
+        RemoteDevice racecar = Bluetooth.getKnownDevice("NXT");
+        
+        if (racecar == null) {
+            System.out.println("No Such device existed");
+            System.exit(1);
+        }
+        BTConnection racecarConnection = Bluetooth.connect(racecar);
+
+        
+        if (racecarConnection == null) {
+            System.out.println("Connection Failed");
+            System.exit(1);
+        }
+       DataOutputStream dos = racecarConnection.openDataOutputStream();
+        try {
+            System.out.println("Sending controller signal");
+            dos.writeInt(CONTROLLER_DEVICE);
+            dos.flush();
+            System.out.println("Sent Controller signal");
+            
+            
+        } catch(Exception ex) {
+            //Do nothing
+        }
+        remote.setRacecarConnection(racecarConnection);
+        remote.setRacecarOutputStream(dos);
+        remote.run();
        
+    }
+
+    public void setRacecarConnection(NXTConnection racecarConnection) {
+        this.racecarConnection = racecarConnection;
+    }
+
+    public void setRacecarOutputStream(DataOutputStream racecarOutputStream) {
+        this.racecarOutputStream = racecarOutputStream;
+    }
+    
+    public RemoteControl() {
+        leftButton   = Button.LEFT;
+        rightButton  = Button.RIGHT;
+        centerButton = Button.ENTER;
+        bottomButton = Button.ESCAPE;
+    }
+    
+    public void run() {
+        while(true) {
+            if(leftButton.isPressed()) {
+            
+            } 
+
+           
+        }
     }
 }
